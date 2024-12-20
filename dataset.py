abc25a9846ecc564d4be44a5c136d84bb35a8b0b
Jarett <jdewbury@uwaterloo.ca>
2024-12-20 10:20:26 -0500

    add updated modality code

--- dataset.py
+++ dataset.py
@@ -1,13 +1,16 @@
 import os
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
-from utils.data_utils import nib_to_numpy
+from utils.data_utils import modality_to_numpy, list_img_paths, list_prostate_paths
 
 class CancerNetPCa:
-    def __init__(self, img_path, mask_path, seed=42, batch_size=10, train_split=0.7, test_split=0.15, prostate=False, transform=None):
+    def __init__(self, img_dir, mask_dir, modality='cdis', channel_idx=0, seed=42, batch_size=10, train_split=0.7, test_split=0.15, prostate=False, transform=None):
         np.random.seed(seed)
         num_workers = os.cpu_count()
         num_workers = max(1, num_workers - 2)
+
+        img_path = list_img_paths(img_dir, modality)
+        mask_path = list_prostate_paths(mask_dir)
         
         dataset_size = len(img_path)
         idxs = np.arange(dataset_size)
@@ -21,9 +24,9 @@ class CancerNetPCa:
         test_idx = idxs[train_size:train_size + test_size]
         val_idx = idxs[dataset_size - val_size:]
 
-        train_dataset = CancerNetPCaDataset(img_path[train_idx], mask_path[:, train_idx], prostate, transform)
-        val_dataset = CancerNetPCaDataset(img_path[val_idx], mask_path[:, val_idx], prostate, transform)
-        test_dataset = CancerNetPCaDataset(img_path[test_idx], mask_path[:, test_idx], prostate, transform)
+        train_dataset = CancerNetPCaDataset(img_path[train_idx], mask_path[:, train_idx], modality, channel_idx, prostate, transform)
+        val_dataset = CancerNetPCaDataset(img_path[val_idx], mask_path[:, val_idx], modality, channel_idx, prostate, transform)
+        test_dataset = CancerNetPCaDataset(img_path[test_idx], mask_path[:, test_idx], modality, channel_idx, prostate, transform)
 
         print(len(train_dataset), len(val_dataset), len(test_dataset))
 
@@ -32,9 +35,11 @@ class CancerNetPCa:
         self.test = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
      
 class CancerNetPCaDataset(Dataset):
-        def __init__(self, img_path, mask_path, prostate=False, transform=None):
+        def __init__(self, img_path, mask_path, modality, channel_idx, prostate=False, transform=None):
             self.img_path = img_path
             self.mask_path = mask_path
+            self.modality = modality
+            self.channel_idx = channel_idx
             self.prostate = prostate
             self.transform = transform
             self.data = self._prepare_data()
@@ -42,7 +47,7 @@ class CancerNetPCaDataset(Dataset):
         def _prepare_data(self):
             data = []
             for img, lesion, prostate in zip(self.img_path, self.mask_path[0], self.mask_path[1]):
-                img_np = nib_to_numpy(img)
+                img_np = modality_to_numpy(img, self.modality, self.channel_idx)
                 prostate_np = np.load(lesion)
                 lesion_np = np.load(prostate)
 
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,10 +1,6 @@
-import os
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
 import nibabel as nib
-import torch
-from collections import OrderedDict
 
 def list_nii_paths(directory):
     """Generator function to iterate over all nii files in a given directory.
@@ -31,53 +27,65 @@ def list_prostate_paths(directory):
     prostate_paths = glob.glob(f'{directory}/**/prostate_mask.npy', recursive=True)
     return np.array([sorted(lesion_paths), sorted(prostate_paths)])
 
-def nib_to_numpy(directory):
-    """Load an image using nibabel, and convert it to a numpy array.
+def list_npy_paths(directory, modality='cdis'):
+    """Generator function to iterate over all npy files in a given directory for a specified modality.
 
     Args:
-        directory: Directory path to convert nib file to numpy array.
+        directory: Directory path to search for nnpy files.
+        modality: Desired modality of interest
+
+    Returns:
+        Sorted array of file paths for each npy file found.
+    """
+    file_paths = glob.glob(f'{directory}/**/{modality.upper()}.npy', recursive=True)
+    return np.array(sorted(file_paths))
+
+def list_img_paths(directory, modality='cdis'):
+    """Generator function to return image paths in a given directory for a specified modality.
+
+    Args:
+        directory: Directory path to search for nnpy files.
+        modality: Desired modality of interest
+
+    Returns:
+        Sorted array of file paths for each image file found.
+    """
+    if modality in ['adc', 'dwi']:
+        return list_npy_paths(directory, modality)
+    elif modality == 'cdis':
+        return list_nii_paths(directory)
+    else:
+        raise ValueError(f"Invalid modality name: {modality}. Choose from 'cdis', 'dwi', or 'adc'.")
+
+def modality_to_numpy(directory, modality='cdis', channel_idx=0):
+    """Load a modality image and convert it to a numpy array.
+
+    Args:
+        directory: Directory path to convert modality file to numpy array.
 
     Returns:
         Numpy array of type uint8.
     """
-    img = nib.load(directory).get_fdata()
-    img = np.nan_to_num(img)
-    img_np = np.array(img).astype(np.uint8)
-    img_f = img_np.astype(float)
-    img_f32 = img_f.astype(np.float32)
-    
-    img_linear_window = [img_f32.min(), img_f32.max()]
+    if modality == 'adc':
+        img_np = np.load(directory, allow_pickle=True)
+        img = np.transpose(img_np, (2, 1, 0))
+    elif modality == 'dwi':
+        img_np = np.load(directory, allow_pickle=True)
+        img = np.transpose(img_np, (3, 2, 1, 0))
+        img = img[:, :, :, channel_idx]
+    elif modality == 'cdis':
+        img_nib = nib.load(directory).get_fdata()
+        img_nib = np.nan_to_num(img_nib)
+        img_np = np.array(img).astype(np.uint8)
+        img_f = img_np.astype(float)
+        img = img_f.astype(np.float32)
+    else:
+        raise ValueError(f"Invalid modality name: {modality}. Choose from 'cdis', 'dwi', or 'adc'.")
 
-    img_clip = np.clip(img_f32, *img_linear_window)
-    
+    img_linear_window = [img.min(), img.max()]
+    img_clip = np.clip(img, *img_linear_window)
     norm_img = (img_clip - img_linear_window[0]) / (
         img_linear_window[1] - img_linear_window[0]
     )
-    
+
     return norm_img
-    
-def load_weights(model, directory):
-    """Load in pre-trained model weights.
-
-    Args:
-        model: Model architecture to load weights.
-        directory: Directory path to model weights.
-
-    Returns:
-        Model with weights loaded.
-    """
-    pretrained_dict = torch.load(directory, map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-    new_state_dict = OrderedDict()
-
-    for k, v in pretrained_dict.items():
-        if 'conv' in k and v.dim() == 5:
-            middle_index = v.shape[2] // 2
-            adapted_v = v[:, :, middle_index, :, :] 
-            if adapted_v.shape == model.state_dict()[k].shape:
-                new_state_dict[k] = adapted_v
-        elif k in model.state_dict() and v.shape == model.state_dict()[k].shape:
-            new_state_dict[k] = v
-
-    model.load_state_dict(new_state_dict, strict=False)
-    
-    return model
